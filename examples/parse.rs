207bc71b5c5d55873cd781b98ca3838ab556c887
Devin R <devin.ragotzy@gmail.com>
2020-03-21 15:53:01 -0400

    add error example

--- examples/parse.rs
+++ examples/parse.rs
@@ -13,10 +13,26 @@ parse_model! {
     [matchers]
     m = r.sub == p.sub && r.obj == p.obj && r.act == p.act
 }
+
+// uncomment to see a compile time error caught by the macro
+// parse_model! {
+//     [request_definition]
+//     r = sub, obj, act
+
+//     [policy_definition]
+//     sub, obj, act
+
+//     [policy_effect]
+//     e = some(where (p.eft == allow))
+
+//     [matchers]
+//     m = r.sub == p.sub && r.obj == p.obj && r.act == p.act
+// }
+
 fn main() {
     let mut model = Model::new();
     model.add_policy("me, data, read");
-    
+
     println!("{:#?}", model);
     println!("{}", model.enforce("me, data, read"));
 }
